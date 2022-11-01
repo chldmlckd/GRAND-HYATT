b8ea729e06b5527cc3212b3c23cc56cd6dd2479b
chldmlckd <106738412+chldmlckd@users.noreply.github.com>
2022-11-01 15:09:22 +0900

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -74,29 +74,25 @@ $(function(){
         $(`.img_box${n}`).html(`<img src="./img/gallery${galleryrandom()}.jpg" alt="">`);
     }
 
-    $('.snsnav li:nth-child(3) a').mouseenter(function(){
+    $('.snsnav li:nth-child(3) a').hover(function(){
         $('.snsnav li:nth-child(3) a img').attr('src','./img/sns_naverPost03.png');
-    });
-    $('.snsnav li:nth-child(3) a').mouseleave(function(){
+    },function(){
         $('.snsnav li:nth-child(3) a img').attr('src','./img/sns_naverPost02.png');
     });
-    $('.snsnav li:nth-child(4) a').mouseenter(function(){
+    $('.snsnav li:nth-child(4) a').hover(function(){
         $('.snsnav li:nth-child(4) a img').attr('src','./img/sns_kakaoCh03.png');
-    });
-    $('.snsnav li:nth-child(4) a').mouseleave(function(){
+    },function(){
         $('.snsnav li:nth-child(4) a img').attr('src','./img/sns_kakaoCh02.png');
     });
 
-    $('.fo_sns li:nth-child(3) a').mouseenter(function(){
+    $('.fo_sns li:nth-child(3) a').hover(function(){
         $('.fo_sns li:nth-child(3) a img').attr('src','./img/sns_naverPost03.png');
+    },function(){
+        $('.fo_sns li:nth-child(3) a img').attr('src','./img/sns_naverPost02.png'); 
     });
-    $('.fo_sns li:nth-child(3) a').mouseleave(function(){
-        $('.fo_sns li:nth-child(3) a img').attr('src','./img/sns_naverPost02.png');
-    });
-    $('.fo_sns li:nth-child(4) a').mouseenter(function(){
+    $('.fo_sns li:nth-child(4) a').hover(function(){
         $('.fo_sns li:nth-child(4) a img').attr('src','./img/sns_kakaoCh03.png');
-    });
-    $('.fo_sns li:nth-child(4) a').mouseleave(function(){
+    },function(){
         $('.fo_sns li:nth-child(4) a img').attr('src','./img/sns_kakaoCh02.png');
     });
 
--- script/main2.js
+++ script/main2.js
@@ -4,29 +4,15 @@ function siteUrl(url){
     }
 }
 $(function(){
-    $('.snsnav li:nth-child(3) a').mouseenter(function(){
-        $('.snsnav li:nth-child(3) a img').attr('src','../img/sns_naverPost03.png');
-    });
-    $('.snsnav li:nth-child(3) a').mouseleave(function(){
-        $('.snsnav li:nth-child(3) a img').attr('src','../img/sns_naverPost02.png');
-    });
-    $('.snsnav li:nth-child(4) a').mouseenter(function(){
-        $('.snsnav li:nth-child(4) a img').attr('src','../img/sns_kakaoCh03.png');
-    });
-    $('.snsnav li:nth-child(4) a').mouseleave(function(){
-        $('.snsnav li:nth-child(4) a img').attr('src','../img/sns_kakaoCh02.png');
-    });
-
-    $('.fo_sns li:nth-child(3) a').mouseenter(function(){
+    $('.fo_sns li:nth-child(3) a').hover(function(){
         $('.fo_sns li:nth-child(3) a img').attr('src','../img/sns_naverPost03.png');
+    },function(){
+        $('.fo_sns li:nth-child(3) a img').attr('src','../img/sns_naverPost02.png'); 
     });
-    $('.fo_sns li:nth-child(3) a').mouseleave(function(){
-        $('.fo_sns li:nth-child(3) a img').attr('src','../img/sns_naverPost02.png');
-    });
-    $('.fo_sns li:nth-child(4) a').mouseenter(function(){
+    $('.fo_sns li:nth-child(4) a').hover(function(){
         $('.fo_sns li:nth-child(4) a img').attr('src','../img/sns_kakaoCh03.png');
-    });
-    $('.fo_sns li:nth-child(4) a').mouseleave(function(){
+    },function(){
         $('.fo_sns li:nth-child(4) a img').attr('src','../img/sns_kakaoCh02.png');
     });
+
 });
